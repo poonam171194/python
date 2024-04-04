60fdf8f96a4e64641f7d45aae10e33d1dc1d52ab
poonam <poonam.rangpariya17@gmail.com>
2024-04-04 13:49:26 +0100

    added my_first_code.py code

---
+++ my_first_code.py
@@ -0,0 +1,9 @@
+env = input("Enter your cloud plateform: ")
+
+if env == "aws" or env == "AWS":
+    print("you are in AWS Platform")
+elif env == "azure":
+    print("You are not in Aws You are in azure Platform")
+else:
+    print("You are in other plateform")
+
