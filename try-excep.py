11aa5039ed23644fd3b87d30510664ec74102594
poonam <poonam.rangpariya17@gmail.com>
2024-04-04 13:40:18 +0100

    added try-excep.py code

---
+++ try-excep.py
@@ -0,0 +1,12 @@
+
+cloud_env = ['aws','gcp','azure']
+
+try:
+    print(cloud_env[2])
+except:
+    print("Out of Range")
+finally:
+    print("It will exicuted always")
+
+print("Code is working")
+   
